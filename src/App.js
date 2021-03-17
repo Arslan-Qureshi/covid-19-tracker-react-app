fdab54404a917549cbfbcabd662aabf43db9ad57
ArslanQureshi <dexter8969@gmail.com>
2021-03-17 17:45:34 +0500

    update app.js
    update the app.js with hello world message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <h1>Hello world finally deployed successfully</h1>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
